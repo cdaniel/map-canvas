c1f9ecad897f7d9c238b60a8b49f829e90ca27e8
Chris Daniel <krzysztof.daniel@gmail.com>
2018-07-31 11:34:36 +0200

    Change Canvas to pallete Better handle the palette attribute styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,68 @@
 import * as React from 'react';
 import './App.css';
 
-import Canvas, {IPaletteComponents} from './components/Canvas';
+import Palette, {IPaletteComponents} from './components/Palette';
 import logo from './logo.svg';
 
 import * as jsPlumb from "jsplumb";
 
 
-
 const params: IPaletteComponents = [
     {
         id: 0, key: "0", label: 'CId', visualElement: <div style={{
-              backgroundColor: 'silver',float:
-            'left', height: 20,width: 20}}/>
+            backgroundColor: 'silver'
+        }}/>
     },
-    {id: 1, key: "01", label: 'Modified By', visualElement: <div style={{
-            backgroundColor: 'yellow',float:
-                'left', height: 20,width: 20}}/>},
-    {id: 2, key: "02", label: 'Modified Date', visualElement: <div style={{
-            backgroundColor: 'red',float:
-                'left', height: 20,width: 20}}/>},
-    {id: 3, key: "03", label: 'Status', visualElement: <div style={{
-            backgroundColor: 'green',float:
-                'left', height: 20,width: 20}}/>},
-    {id: 4, key: "04", label: 'Status > Type', visualElement: <div style={{
-            backgroundColor: 'black',float:
-                'left', height: 20,width: 20}}/>},
-    {id: 5, key: "05", label: 'Title', visualElement: <div style={{
-            backgroundColor: 'blue',float:
-                'left', height: 20,width: 20}}/>},
-    {id: 6, key: "06", label: 'Type', visualElement: <div style={{
-            backgroundColor: 'purple',float:
-                'left', height: 20,width: 20}}/>},
-    {id: 7, key: "087", label: 'Type > Status', visualElement: <div style={{
-            backgroundColor: 'gray',float:
-                'left', height: 20,width: 20}}/>}
+    {
+        id: 1, key: "01", label: 'Modified By', visualElement: <div style={{
+            backgroundColor: 'yellow'
+        }}/>
+    },
+    {
+        id: 2, key: "02", label: 'Modified Date', visualElement: <div style={{
+            backgroundColor: 'red'
+        }}/>
+    },
+    {
+        id: 3, key: "03", label: 'Status', visualElement: <div style={{
+            backgroundColor: 'green'
+        }}/>
+    },
+    {
+        id: 4, key: "04", label: 'Status > Type', visualElement: <div style={{
+            backgroundColor: 'black'
+        }}/>
+    },
+    {
+        id: 5, key: "05", label: 'Title', visualElement: <div style={{
+            backgroundColor: 'blue'
+        }}/>
+    },
+    {
+        id: 6, key: "06", label: 'Type', visualElement: <div style={{
+            backgroundColor: 'purple'
+        }}/>
+    },
+    {id: 7, key: "087", label: 'Type > Status', visualElement: <div style={{backgroundColor: 'gray'}}/>}
 ];
 
 const jsPlumbInstance = jsPlumb.jsPlumb.getInstance();
+
 class App extends React.Component {
-  public render() {
-    return (
-      <div style={{display:'flex',height:'100%', flexFlow:'column'}}>
-        <header className="App-header" style={{flex:"0 1 auto"}}>
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-          <div style={{display:'flex', flex:"1 1 auto",}}>
-              <Canvas components={params} jsPlumbInstance={jsPlumbInstance}/>
-              <div style={{background:'red', flex:'1 1 auto', minHeight:500, minWidth:600}}>blah</div>
-          </div>
-      </div>
-    );
-  }
+    public render() {
+        return (
+            <div style={{display: 'flex', height: '100%', flexFlow: 'column'}}>
+                <header className="App-header" style={{flex: "0 1 auto"}}>
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <div style={{display: 'flex', flex: "1 1 auto",}}>
+                    <Palette components={params} jsPlumbInstance={jsPlumbInstance}/>
+                    <div style={{background: 'red', flex: '1 1 auto', minHeight: 500, minWidth: 600}}>blah</div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Palette.tsx
@@ -0,0 +1,92 @@
+import * as React from 'react';
+
+import * as jsPlumb from './../../node_modules/jsplumb';
+
+import {jsPlumbInstance} from "jsplumb";
+import {ReactElement} from "react";
+
+
+interface IPaletteComponent {
+    id: number;
+    key: string;
+    label: string;
+    visualElement: ReactElement<HTMLDivElement>;
+};
+
+export interface IPaletteComponents extends Array<IPaletteComponent> {
+};
+
+export interface IProps {
+    components: IPaletteComponents,
+    jsPlumbInstance: jsPlumbInstance
+}
+
+export default class Canvas extends React.Component<IProps, object> {
+
+    private components: IPaletteComponents;
+    private jsPlumbInstance: jsPlumbInstance;
+
+    constructor(props: IProps) {
+        super(props);
+        this.components = props.components;
+        this.jsPlumbInstance = props.jsPlumbInstance;
+    }
+
+    public renderComponent(component: IPaletteComponent) {
+        const key = component.key;
+        const style = component.visualElement.props.style;
+        if(style){
+            style.cssFloat = 'left';
+            style.height = '20px';
+            style.width = '20px';
+        }
+        return <div ref={this.makeDraggable.bind(this, key)} key={key} style={{margin: 10}}>
+            <div>{component.visualElement}</div>
+            <span>&nbsp;{component.label}</span></div>;
+    }
+
+    public makeDraggable(type: string, input: {}) {
+
+        if (input === null) {
+            // noop - component was destroyed, no need to worry about draggable
+            return;
+        }
+        const d = this.jsPlumbInstance.draggable(input, {
+            clone: true,
+            grid: [
+                '10', '10'
+            ],
+            ignoreZoom: true
+        } as jsPlumb.DragOptions);
+        return d;
+    }
+
+    public renderComponents(components: IPaletteComponents) {
+        const result = [];
+        for (let i = 0; i < components.length; i++) {
+            const component: IPaletteComponent = this.components[i];
+            result.push(this.renderComponent(component));
+        }
+        return result;
+    }
+
+    public render() {
+        const renderedComponents = this.renderComponents(this.components);
+        return (
+            <div style={{
+                alignSelf: 'flex-start',
+                borderColor: 'gray',
+                borderStyle: 'dotted',
+                borderWidth: 1,
+                margin: 3,
+                minWidth: 170,
+                width: 170
+            }}>
+                <div style={{marginLeft: 3}}>
+                    {renderedComponents}
+                </div>
+            </div>
+        );
+    }
+
+}
